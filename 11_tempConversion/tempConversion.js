c87e5f8e7c7f1b87062706b784df503524249fee
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-08-17 19:35:06 -0700

    parse as float toFixed for decimal place reduce fraction in Temp to 1.8

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return parseFloat(((temp-32)/1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return parseFloat(((1.8*temp)+32).toFixed(1));
 };
-
+console.log(convertToFahrenheit(-10))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
