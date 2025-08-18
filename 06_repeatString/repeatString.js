4c018487ae6c09a45a8ad2e9989fb0de95c78037
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-08-17 17:54:21 -0700

    repeat string based on number of iterations

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringValue,numOfRepeats) {
+    let returnValue ="";
+    if(numOfRepeats<=0)
+        return Error;
+    for(let i =1;i<=numOfRepeats;i++)
+        returnValue+=stringValue;     
+    return returnValue
 };
 
 // Do not edit below this line
