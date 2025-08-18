1d7da602ab0094ebd71282f6c87b6a992b625121
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-08-17 18:08:16 -0700

    split array into map looking at end of the index then join back

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(stringValue) {
+    return stringValue.split("").map((char,i,stringValue)=> stringValue[stringValue.length-1-i]).join('')
 };
 
 // Do not edit below this line
