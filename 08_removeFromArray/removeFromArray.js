db854ae8248880e18c950d99be9a0e066a319bd3
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-08-17 18:26:37 -0700

    remove from array while loop multiple index included and multiple parameters to remove

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...remove) {
+    for(const test of remove){
+        while(arr.includes(test)){           
+            arr.splice(arr.indexOf(test),1);
+        }
+    }
+    return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
