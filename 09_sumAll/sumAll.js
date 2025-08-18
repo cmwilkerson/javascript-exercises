1df5031696ca91cbff6139df35b3be3464df21e7
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-08-17 19:10:46 -0700

    adding error logic and creation of array for reduce functionality

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(boundOne,boundTwo) {
+    if(
+    typeof(boundOne)=='string'|| typeof(boundTwo)=='string'||
+    !Number.isInteger(boundOne)||!Number.isInteger(boundTwo)
+    ||boundOne<0 || boundTwo<0
+     ) return "ERROR";
+    if(boundOne===boundTwo)
+        return boundOne;
+    let upperBound =0;
+    let lowerBound =0;
+    if(boundOne>boundTwo){
+        upperBound=boundOne;
+        lowerBound=boundTwo;
+    }else{
+        upperBound=boundTwo;
+        lowerBound=boundOne;
+    }
+    let arr=[...Array(upperBound+-lowerBound+1).keys()].map(num => lowerBound+num);
+    console.log(arr);
+return ( arr.reduce((acc,current) =>{
+    return acc+current;
+},0));
 };
 
 // Do not edit below this line
