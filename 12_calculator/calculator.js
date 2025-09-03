3e10b983588607f807688840b98a36d23b7b12ed
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-09-02 20:00:34 -0700

    Calc Problems Completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+  return num1- num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total,currentNum)=> total+currentNum,0)
+};
+const multiply = function(arr) {
+return arr.reduce((total,currentNum)=> total*currentNum,1)
 };
 
-const multiply = function() {
-
+const power = function(num1,num2) {
+	return num1**num2;
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
+  let aggregate=1;
+	for(let i=num;i > 0; i--)
+    {
+    aggregate*=i;
+  }
+  return aggregate;
 
-const factorial = function() {
-	
 };
+console.log(factorial(5))
+
 
 // Do not edit below this line
 module.exports = {
