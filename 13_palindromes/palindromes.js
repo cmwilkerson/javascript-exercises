2367dbf236a11cad82aa23f7f9b44ccb4722083c
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-09-02 20:32:04 -0700

    Palindromes Array functions

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (palinString) {
+    allowedChars="abcdefghijklmnopqrstuvwxyz0987654321"
+    let stringToConsider=palinString.toLowerCase().split("").filter((charInput)=> allowedChars.includes(charInput)).join("")
+    reversedString=stringToConsider.split("").reverse().join("");
+    return stringToConsider===reversedString;
 };
+console.log(palindromes("hEll$$o!!"));
 
 // Do not edit below this line
 module.exports = palindromes;
