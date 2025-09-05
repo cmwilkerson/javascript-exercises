1cccc87eefc77683c47dfc61f34343e6326d378c
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-09-04 21:23:31 -0700

    found the oldest tried new methods

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+   let arrayYears= arr.map(person => [person.yearOfBirth,person.yearOfDeath ??= new Date().getFullYear()]);// returns an array of the years creating current year as death year if undefined with ??=
+    const arrAge=arrayYears.map(personAge => personAge[1]-personAge[0]);// gets numerical value of years to compare
+    return arr[arrAge.indexOf(Math.max(...arrAge))];//uses the postion of the max value within the array that gets spread to find the index of the original object to return
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
