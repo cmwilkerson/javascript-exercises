b09dda3abe6155817b50d92207bae40c1b1558f4
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-09-04 20:28:30 -0700

    Fib seq completed with string inclusion

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num <0)
+        return "OOPS";
+     if (num == 0)
+        return 0;
+    if (num == 1|| num ==2)
+        return 1;
+    let [previous,next]= [1,1];
+    for(i=2;i<=num;i++){
+        [previous,next]=[next,previous+next]
+        console.log(`i=${i} Previous:${previous} Next:${next}`)
+       
+    }
+return previous;
 };
 
 // Do not edit below this line
